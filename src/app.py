5d3d363aa8f7a0c785f51e803281fc9a69f7eb6a
Daniel Sanchez <danielandresanchez2004@gmail.com>
2023-06-08 13:33:30 -0500

    JFL

--- src/app.py
+++ src/app.py
@@ -268,4 +268,4 @@ def send_password_reset_email(email, token):
     
 
 if __name__ == "__main__":
-    app.run(debug=True , host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
--- src/db.py
+++ src/db.py
@@ -1,48 +1,29 @@
+# db.py
+
 import mysql.connector
 
-class MySQLDatabase:
-    def __init__(self, host, user, password, database):
-        self.host = host
-        self.user = user
-        self.password = password
-        self.database = database
+def create_tables():
+    connection = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="",
+        database="baño"
+    )
+    cursor = connection.cursor()
 
-    def get_connection(self):
-        return mysql.connector.connect(
-            host=self.host,
-            user=self.user,
-            password=self.password,
-            database=self.database
+    # Define la sentencia SQL para crear la tabla users
+    create_users_table_query = '''
+        CREATE TABLE IF NOT EXISTS users (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            email VARCHAR(255) NOT NULL,
+            password VARCHAR(255) NOT NULL
         )
+    '''
 
-    def create_tables(self):
-        connection = self.get_connection()
-        cursor = connection.cursor()
 
-        # Define la sentencia SQL para crear la tabla users
-        create_users_table_query = '''
-            CREATE TABLE IF NOT EXISTS users (
-                id INT AUTO_INCREMENT PRIMARY KEY,
-                email VARCHAR(255) NOT NULL,
-                password VARCHAR(255) NOT NULL
-            )
-        '''
+    # Ejecuta las sentencias SQL para crear las tablas
+    cursor.execute(create_users_table_query)
+    connection.commit()
 
-        # Define la sentencia SQL para crear la tabla contacto
-        create_contacto_table_query = '''
-            CREATE TABLE IF NOT EXISTS contacto (
-                id INT AUTO_INCREMENT PRIMARY KEY,
-                nombre VARCHAR(255) NOT NULL,
-                mensaje VARCHAR(255) NOT NULL
-            )
-        '''
-
-        # Ejecuta las sentencias SQL para crear las tablas
-        cursor.execute(create_users_table_query)
-        cursor.execute(create_contacto_table_query)
-        connection.commit()
-
-        cursor.close()
-        connection.close()
-        
-        
+    cursor.close()
+    connection.close()
